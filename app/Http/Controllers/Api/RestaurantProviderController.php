6bb11c2610663e53250a414b89ad1a090a1a5a9a
mbozkurt020 <mehmetbozkurt@plusclouds.com>
2026-01-18 19:40:15 +0300

    entegra

--- app/Http/Controllers/Api/RestaurantProviderController.php
+++ app/Http/Controllers/Api/RestaurantProviderController.php
@@ -67,22 +67,20 @@ class RestaurantProviderController extends Controller
             }
         } else {
             $response = RestaurantProviderService::updateRestaurantProvider($restaurant->restaurant_id, $provider->provider_id, $request->all());
-return $response;
-            if ($response->detail->status) {
-                $response = $response->detail->integrations[0];
+
+            if ($response['detail']['status']) {
+                $response = $response['detail']['integrations'][0];
 
                 RestaurantProvider::where('restaurant_id', $restaurantId)->where('provider_id', $providerId)->update([
                     'restaurant_id' => $restaurantId,
                     'provider_id' => $providerId,
-                    'name' => $response->name,
-                    'slug' => $response->slug,
-                    'status' => $response->status,
-                    'is_eco_friendly' => $response->isEcoFriendly,
-                    'do_not_knock' => $response->doNotKnock,
-                    'drop_off_at_door' => $response->dropOffAtDoor,
-                    'auto_approve' => $response->otomatikOnay,
-                    'service' => $response->service,
-                    'information' => json_encode($response->information),
+                    'status' => $response['status'],
+                    'is_eco_friendly' => $response['isEcoFriendly'],
+                    'do_not_knock' => $response['doNotKnock'],
+                    'drop_off_at_door' => $response['dropOffAtDoor'],
+                    'auto_approve' => $response['otomatikOnay'],
+                    'service' => $response['service'],
+                    'information' => json_encode($response['information']),
                 ]);
 
                 return ['success' => true,'data' => $restaurant];
